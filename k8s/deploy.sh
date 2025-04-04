10192c2a8dd8b299253695c2df3ebbd78453a080
karchevskii <dinar.karchevskii.de@gmail.com>
2025-04-04 13:59:03 +0200

    k8s: full setup

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,8 +1,5 @@
-minikube addons enable ingress
-sleep 5
-
 kubectl apply --server-side -f https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.25/releases/cnpg-1.25.1.yaml
-sleep 5
+sleep 25
 
 # Namespace
 kubectl apply -f namespace.yaml
@@ -31,21 +28,3 @@ kubectl apply -f frontend.yaml
 
 #Ingress
 kubectl apply -f ingress.yaml
-
-sleep 30
-
-# Run port-forwards in parallel
-kubectl port-forward -n tictactoe svc/users-db-cluster-rw 5432:5432 &
-kubectl port-forward -n tictactoe svc/game-history-db-cluster-rw 5433:5432 &
-kubectl port-forward -n tictactoe svc/redis-primary 6379:6379 &
-minikube tunnel &
-
-# Save all background process IDs
-pids=($!)
-
-# Add trap to kill background processes on script exit
-trap "kill ${pids[*]} 2>/dev/null" EXIT
-
-# Wait for Ctrl+C to terminate
-echo "Port forwarding in progress. Press Ctrl+C to stop."
-wait
